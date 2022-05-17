3f81ad23b51741ea013b893f27cfd789e1793733
Jack Myers <jmyers@launchpointeps.com>
2022-05-17 12:58:20 -0700

    try cross-compile again

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 cd "${SRC_DIR}"
 export IPOPT_DIR="$PREFIX"
 export NP_INC="${SP_DIR}/numpy/core/include/"
 echo "PYTHON TARGET=${PYTHON}"
-meson setup build --prefix="${PREFIX}" -Dipopt_dir="${IPOPT_DIR}" -Dincdir_numpy="${NP_INC}" -Dpython_target="${PYTHON}"
+meson setup build --prefix="${PREFIX}" -Dipopt_dir="${IPOPT_DIR}" -Dincdir_numpy="${NP_INC}" -Dpython_target="${PYTHON}" "${MESON_ARGS}"
 meson install -C build
