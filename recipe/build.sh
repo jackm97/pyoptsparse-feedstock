dc9795f66dd1be470a1f25ff5d866df1d36829c0
Jack Myers <jmyers@launchpointeps.com>
2022-05-17 01:31:54 -0700

    MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.16.12.18.44

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
-cd ${SRC_DIR}
-IPOPT_DIR=${PREFIX} ${PYTHON} -m pip install . -vv
+cd "${SRC_DIR}"
+export IPOPT_DIR="$PREFIX"
+export NP_INC="${SP_DIR}/numpy/core/include/"
+echo "PYTHON TARGET=${PYTHON}"
+meson setup build --prefix="${PREFIX}" -Dipopt_dir="${IPOPT_DIR}" -Dincdir_numpy="${NP_INC}" -Dpython_target="${PYTHON}"
+meson install -C build
