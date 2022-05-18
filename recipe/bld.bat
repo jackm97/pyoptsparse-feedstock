909644f9cd94d03b551fa95ca83c6e07867c5d46
Jack Myers <jmyers@launchpointeps.com>
2022-05-17 22:49:46 -0700

    [ci skip] ***NO_CI*** updated meta.yml and tested windows locally

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,10 @@
 @echo on
 
 cd %SRC_DIR%
-set IPOPT_DIR=%PREFIX%
-set NP_INC=%SP_DIR%/numpy/core/include/
-meson setup build --prefix=%PREFIX% -Dipopt_dir=%IPOPT_DIR% -Dincdir_numpy=%NP_INC% -Dpython_target=%PYTHON} %MESON_ARGS%
+set IPOPT_DIR=%LIBRARY_PREFIX%
+set NP_INC=%SP_DIR%\numpy\core\include
+set CC=cl
+set FC=flang
+set CC_LD=link
+meson setup build --prefix=%PREFIX% -Dipopt_dir=%IPOPT_DIR% -Dincdir_numpy=%NP_INC% -Dpython_target=%PYTHON%
 meson install -C build
