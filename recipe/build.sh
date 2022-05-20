f1233e400def780b408a8757715a4903f8d0222f
Jack Myers <jmyers@launchpointeps.com>
2022-05-20 13:26:21 -0700

    switch to pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,16 @@
 set -eox pipefail
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+
 EXTRA_FLAGS=""
-IPOPT_DIR="$PREFIX"
 NP_INC="${SP_DIR}/numpy/core/include/"
 echo "PYTHON TARGET=${PYTHON}"
+
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
   # Add pkg-config to cross-file binaries since meson will disable it
   # See https://github.com/mesonbuild/meson/issues/7276
   echo "[binaries]" >> $BUILD_PREFIX/meson_cross_file.txt
-  echo "pkg-config = '$(which pkg-config)'" >> $BUILD_PREFIX/meson_cross_file.txt
+  echo "pkg-config = '$(which pkg-config)'" >> "$BUILD_PREFIX"/meson_cross_file.txt
   # Use Meson cross-file flag to enable cross compilation
   EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
   NP_INC=""
@@ -20,5 +21,6 @@ fi
 export PKG_CONFIG=$(which pkg-config)
 
 cd "${SRC_DIR}"
-meson setup build --prefix="${PREFIX}" -Dipopt_dir="${IPOPT_DIR}" -Dincdir_numpy="${NP_INC}" -Dpython_target="${PYTHON}" ${EXTRA_FLAGS}
-meson install -C build
+export MESON_ARGS="-Dipopt_dir=${PREFIX} -Dincdir_numpy=${NP_INC} -Dpython_target=${PYTHON} ${EXTRA_FLAGS}"
+python -m build -n -x .
+pip install --prefix "${PREFIX}" --no-deps --no-index --find-links dist pyoptsparse
